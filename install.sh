d01bfe209ff87599e7a2e58474bc7e2abe9fce3f
Manish Kumar <manishprivet@protonmail.com>
2021-07-26 17:13:56 +0530

    feat(scripts): added support for global and local installs
    Signed-off-by: Manish Kumar <manishprivet@protonmail.com>

--- install.sh
+++ install.sh
@@ -6,6 +6,31 @@ GREEN='\033[0;32m'
 YELLOW='\033[0;33m'  
 OFF='\033[0m' 
 
+LOCATION=~/.local/share
+
+PLACEHOLDER="__LOCATION__"
+
+if [ $# -eq 0 ]
+then
+  echo -e "${RED}Missing Theme Name${OFF}"
+  echo ""
+  exit 0
+fi
+
+echo $2
+
+if [[ "$2" == "--global" ]] ; then
+  echo -e "${YELLOW}Installing Wallpaper Globally${OFF}"
+  echo ""
+  LOCATION=/usr/share
+
+  if [[ $EUID -ne 0 ]]; then
+    echo -e "${RED}You must run the script as root to install the Wallpaper globally${OFF}"
+    echo ""
+    exit 1
+  fi
+fi
+
 # Check up and clean old verisons
 echo "Checking and cleaning up old $1 files"
 
@@ -19,15 +44,16 @@ fi
 
 echo "Adding $1 to wallpapers ..."
 
-# Create wallpapers directory if doesn't exist
-sudo mkdir -p /usr/share/backgrounds/gnome
+# Create directories if they doesn't exist
+mkdir -p $LOCATION/backgrounds/gnome
+mkdir -p $LOCATION/gnome-background-properties
 
 # Download files from the zip repo
 echo -e "${YELLOW}Downloading Files...${OFF}"
 echo ""
-status=$(curl -LJ https://cdn.manishk.dev/$1.zip -o ./$1.zip --write-out %{http_code} --progress-bar)
+status=$(curl -LJ https://cdn.manishk.dev/v2%2F$1.zip -o ./$1.zip --write-out %{http_code} --progress-bar)
 
-# If file doesn't exist, exit with an error
+# # If file doesn't exist, exit with an error
 if [[ "$status" -ne 200 ]] ; then
   echo ""
   echo -e "${RED}$1 theme doesn't exist. Make sure you spelled it correctly.${OFF}"
@@ -35,27 +61,30 @@ if [[ "$status" -ne 200 ]] ; then
   exit 0
 fi
 
-# Unzip the file
+# # Unzip the file
 unzip -q $1.zip -d ./$1
 
 # If old directory exists, delete it
-if [ -d "/usr/share/backgrounds/gnome/$1-timed" ]; then
-    echo ""
-    echo -e "${YELLOW}Removing old $1 wallpaper...${OFF}"
-    ./uninstall.sh $1
-    echo -e "${GREEN}Old files removed${OFF}"
+if [ -d "$LOCATION/backgrounds/gnome/$1-timed" ]; then
+  echo ""
+  echo -e "${YELLOW}Removing old $1 wallpaper...${OFF}"
+  ./uninstall.sh $1 $2
+  echo -e "${GREEN}Old files removed${OFF}"
 fi
 
 
 cd $1
 
+sed -i "s+$PLACEHOLDER+$LOCATION+g" $1.xml
+sed -i "s+$PLACEHOLDER+$LOCATION+g" $1-timed.xml
+
 # Create required directories
-sudo mkdir /usr/share/backgrounds/gnome/$1-timed
-sudo cp $1*.jp* /usr/share/backgrounds/gnome/$1-timed
-sudo cp $1-timed.xml /usr/share/backgrounds/gnome
-sudo cp $1.xml /usr/share/gnome-background-properties
+mkdir $LOCATION/backgrounds/gnome/$1-timed
+cp $1*.jp* $LOCATION/backgrounds/gnome/$1-timed
+cp $1-timed.xml $LOCATION/backgrounds/gnome
+cp $1.xml $LOCATION/gnome-background-properties
 echo ""
-echo -e "${GREEN}Added $1 dynamic wallpaper!"
+echo -e "${GREEN}Added $1 dynamic wallpaper!${OFF}"
 
 # Cleanup zip file and extracted folder
 echo "Cleaning up ..."
--- uninstall.sh
+++ uninstall.sh
@@ -1,8 +1,27 @@
 #! /bin/bash
 
-echo "Removing $1 from installed wallpapers ..."
-cd $i
-sudo rm -rf /usr/share/backgrounds/gnome/$1-timed
-sudo rm -rf /usr/share/backgrounds/gnome/$1-timed.xml  
-sudo rm -rf /usr/share/gnome-background-properties/$1.xml
-echo "Removed $1 dynamic wallpaper!"
+# Adding some splash of colours
+RED='\033[0;31m'
+GREEN='\033[0;32m'        
+YELLOW='\033[0;33m'  
+OFF='\033[0m' 
+
+LOCATION=~/.local/share
+
+if [[ "$2" == "--global" ]] ; then
+  echo -e "${YELLOW}Uninstalling Wallpaper Globally${OFF}"
+  echo ""
+  LOCATION=/usr/share
+
+  if [[ $EUID -ne 0 ]]; then
+    echo -e "${RED}You must run the script as root to install the Wallpaper globally${OFF}"
+    echo ""
+    exit 1
+  fi
+fi
+
+echo -e "${YELLOW}Removing $1 from installed wallpapers ...${OFF}"
+rm -rf $LOCATION/backgrounds/gnome/$1-timed
+rm -rf $LOCATION/backgrounds/gnome/$1-timed.xml  
+rm -rf $LOCATION/gnome-background-properties/$1.xml
+echo -e "${GREEN}Removed $1 dynamic wallpaper!${OFF}"
